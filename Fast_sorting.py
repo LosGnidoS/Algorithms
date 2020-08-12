8500f84aa82f4f8aed8a4d648af583c208e52e17
Kirill Shvedov <github.shvedov@gmail.com>
2020-08-12 21:44:15 +0300

    Update Fast_sorting.py

--- Fast_sorting.py
+++ Fast_sorting.py
@@ -12,9 +12,15 @@ def quickSort(array):
 		return array
 	else:
 		pivot = array[0]
-		less = [i for i in array[1:] if i <= pivot]
-		greater = [i for i in array[1:] if i > pivot]
-		return quickSort(less) + [pivot] + quickSort(greater)
+		
+		less = [i for i in array[1:]
+			if i <= pivot]
+		
+		greater = [i for i in array[1:]
+			   if i > pivot]
+		
+		return quickSort(less) + [pivot]\
+	+ quickSort(greater)
 
 arr = [56,4,5,7,8,4,6,82,56,4,63,7,56]
 
